15f60ab5f2da679bb79a306c2b79c22701fa1c12
SPOOK_EXE <91944990+SPOOKEXE@users.noreply.github.com>
2023-09-24 08:25:58 +0930

    small-additions
    pre-drone model

--- src/ReplicatedStorage/DroneSystemShared.lua
+++ src/ReplicatedStorage/DroneSystemShared.lua
@@ -2,4 +2,13 @@
 -- // Module // --
 local Module = {}
 
+function Module:IsDroneBusy( DroneModel )
+	return DroneModel:GetAttribute('IsBusy')
+end
+
+function Module:CanPlayerControlDrone( LocalPlayer, DroneModel )
+	local Humanoid = LocalPlayer.Character and LocalPlayer.Character:FindFirstChildWhichIsA('Humanoid')
+	return Humanoid and Humanoid.Health > 0 and (not Module:IsDroneBusy( DroneModel ))
+end
+
 return Module
--- src/StarterPlayerScripts/DroneSystemClient.lua
+++ src/StarterPlayerScripts/DroneSystemClient.lua
@@ -1,9 +1,25 @@
 
+local Players = game:GetService('Players')
+local LocalPlayer = Players.LocalPlayer
+
+local PlayerModule = require(LocalPlayer:WaitForChild('PlayerScripts'):WaitForChild('PlayerModule'))
+local Controls = PlayerModule:GetControls()
+
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local DroneSystemShared = require(ReplicatedStorage:WaitForChild("DroneSystemShared"))
 
+local CurrentCamera = workspace.CurrentCamera
+
 -- // Module // --
 local Module = {}
+
+function Module:EnableMovement()
+	Controls:Enable()
+end
+
+function Module:DisableMovement()
+	Controls:Disable()
+end
 
 function Module:Start()
 
